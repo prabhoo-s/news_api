54146ca68f7ac27a437d66ff94c1c76d6c272980
prabhu <psomasundaram_contractor@mtb.com>
2021-03-16 11:53:48 -0400

    added date info to top headlines

--- news_api/lib/Presentation/widgets/th_tab_row_text.dart
+++ news_api/lib/Presentation/widgets/th_tab_row_text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 import 'package:news_api/Data/Models/top_headlines.dart';
 import 'package:news_api/Styles.dart';
 
@@ -9,6 +10,8 @@ class THTabRowText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String _publishedDate =
+        DateFormat.yMMMd().format(DateTime.parse(article.publishedAt));
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 5),
       child: Column(
@@ -20,6 +23,10 @@ class THTabRowText extends StatelessWidget {
             style: Styles.appNormal,
           ),
           SizedBox(height: 5.0),
+          Text(
+            _publishedDate,
+            style: Styles.appItalicsSemiLight,
+          ),
           Text(
             'By ${article.author}',
             overflow: TextOverflow.ellipsis,
